d81229b431b1c83169fec78837ae0c8f97c5ece4
fardanaljihad <fardanaljihad.18@gmail.com>
2025-08-23 18:44:04 +0700

    Add the application key

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,6 @@
 {
     'name': "The Real Estate Advertisement",
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['base'],  # The only necessary framework module for now is base.
+    'application': True  # The key so that the module appears when the ‘Apps’ filter is on.
 }
