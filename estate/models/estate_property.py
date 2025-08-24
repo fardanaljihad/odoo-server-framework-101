521e7c6e0018f9708da0cb79c32525a8feeca939
fardanaljihad <fardanaljihad.18@gmail.com>
2025-08-24 11:29:10 +0700

    Set fields, attributes, and view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,10 +7,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), months=3), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +20,17 @@ class EstateProperty(models.Model):
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
+    state = fields.Selection(
+        string='Status',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='new',
+        copy=False,
+        required=True
+    )
+    active = fields.Boolean(default=True)  # Reserved Fields
