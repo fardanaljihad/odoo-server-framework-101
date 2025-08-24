80dc22000176a600e70c378b62d3d772d7111261
fardanaljihad <fardanaljihad.18@gmail.com>
2025-08-24 09:08:51 +0700

    Add access to the data for specific groups of users

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,5 +2,9 @@
     'name': "The Real Estate Advertisement",
     'version': '1.0',
     'depends': ['base'],  # The only necessary framework module for now is base.
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'application': True  # The key so that the module appears when the ‘Apps’ filter is on.
 }
