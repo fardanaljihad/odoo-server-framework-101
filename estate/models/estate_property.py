94fcf30de175547aaa5f10c245f6e1809226b83a
fardanaljihad <fardanaljihad.18@gmail.com>
2025-08-25 07:34:34 +0700

    Add a custom list view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,14 +4,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(default=fields.Date.add(fields.Date.today(), months=3), copy=False)
+    date_availability = fields.Date(
+        string='Available From',
+        default=fields.Date.add(fields.Date.today(), months=3), 
+        copy=False
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(copy=False, readonly=True)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -33,4 +37,4 @@ class EstateProperty(models.Model):
         copy=False,
         required=True
     )
-    active = fields.Boolean(default=True)  # Reserved Fields
+    active = fields.Boolean(default=True)
