ecb41995e1dd5cdd5d48b2b2516b79e09c7ea4bf
fardanaljihad <fardanaljihad.18@gmail.com>
2025-08-27 18:40:50 +0700

    Add estate property tag model with Many2many relation to estate property model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
         'views/estate_menus.xml'
     ],
     'application': True  # The key so that the module appears when the ‘Apps’ filter is on.
