1f32bdd43b0898ffe67e9fcff7016b8a4f2c051a
fardanaljihad <fardanaljihad.18@gmail.com>
2025-09-01 19:18:55 +0700

    Add computed field best price

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -44,8 +44,17 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many('estate.property.tag', string='Property Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
     total_area = fields.Integer(compute='_compute_total')
+    best_price = fields.Float(compute='_compute_best_price', string='Best Offer')
 
     @api.depends('living_area', 'garden_area')
     def _compute_total(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = 0.0
