78aadc8d3e2ac9973b38e442b59a0da1a9f6f810
fardanaljihad <fardanaljihad.18@gmail.com>
2025-09-02 17:22:21 +0700

    Add computed field date deadline with inverse

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -11,4 +11,18 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7, string='Validity (days)')
+    # inverse method is called when saving the record, while the compute method is called at each change of its dependencies.
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', string='Deadline')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = record.create_date or fields.Date.today()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+
+    def _inverse_date_dateline(self):
+        for record in self:
+            diff = record.date_deadline - fields.Date.to_date(record.create_date)
+            record.validity = diff.days
     
