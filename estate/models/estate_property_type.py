672e8ba791939f667b9eca2106071116f669ac46
fardanaljihad <fardanaljihad.18@gmail.com>
2025-09-04 07:28:23 +0700

    Add an inline list view

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _description = "Real Estate Property Type"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
         ('check_name', 'UNIQUE(name)', 'Type must be unique.')
