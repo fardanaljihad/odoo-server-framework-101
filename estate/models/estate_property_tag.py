46b5c624c75d9833077f59ecf2d1ab030c5590af
fardanaljihad <fardanaljihad.18@gmail.com>
2025-09-04 08:45:08 +0700

    Add widget options

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
     _sql_constraints = [
         ('check_name', 'UNIQUE(name)', 'Tag name must be unique.')
